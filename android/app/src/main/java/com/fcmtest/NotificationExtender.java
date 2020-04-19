d6d2ccf805ead1b25745e69814f836a2e161fed1
Yoga <Mryoga1995@gmail.com>
2020-04-19 20:05:57 +0700

    add NotificationExtender

---
+++ android/app/src/main/java/com/fcmtest/NotificationExtender.java
@@ -0,0 +1,21 @@
+package com.fcmtest;
+
+import android.content.Intent;
+
+import com.onesignal.NotificationExtenderService;
+import com.onesignal.OSNotificationReceivedResult;
+
+/**
+ * Created on : 19/04/20
+ * Author     : arioki
+ * Name       : Yoga Setiawan
+ * GitHub     : https://github.com/arioki
+ */
+public class NotificationExtender extends NotificationExtenderService {
+    @Override
+    protected boolean onNotificationProcessing(OSNotificationReceivedResult notification) {
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i);
+        return false;
+    }
+}
